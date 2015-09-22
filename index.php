184f53c4e40c3681730e60733a32cc035014d280
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-22 22:52:38 +0300

    Adding courts

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+
+<!--
+	HTML5 Reset: https://github.com/murtaugh/HTML5-Reset
+	Free to use
+-->
+
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="dsa">
+	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine (even in intranet) -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DSA</title>
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0"/>
+	<!-- concatenate and minify for production -->
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assets/css/reset.css" />
+	<link rel="stylesheet" href="assets/css/desktop.css" />
+
+	<!-- Target iPhone -->
+	<link rel="stylesheet" href="assets/css/handheld.css" media="(max-device-width:480px)" />
+
+	<!-- Target iPad -->
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-device-width:768px) and (max-device-width:1024px)" />
+
+	<!-- Target Galaxy Tab -->
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-width:1280px) and (max-width:1280px)" />
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-width:800px) and (max-width:800px)" />
+
+	<link href='https://fonts.googleapis.com/css?family=Indie+Flower|Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Marck+Script&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Philosopher&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Leckerli+One|Rokkitt:700,400|Luckiest+Guy" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	 <link href='https://fonts.googleapis.com/css?family=Lobster|Marmelad|Bad+Script&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lobster|Marmelad|Bad+Script|Poiret+One&subset=latin,cyrillic' rel='stylesheet' type='text/css'>	
+ 	<link href='https://fonts.googleapis.com/css?family=Istok+Web&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+ 	<link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+ <script src="assets/js/libs/prefixfree.min.js"></script>
+	 <script src="assets/js/libs/modernizr-2.8.3.dev.js"></script>
+</head>
+<body>
+<header>
+	<a class="emblem" href="index.php"><img src="images/emblem_ukr1.png" title="Судова влада України" alt="Судова влада України"></a>
+	<h1><a href="#">Судова влада України</a></h1>
+	<nav id="topnav">
+		<ul>
+			<li><a href="index.php">Суды</a></li>
+			<li><a href="aboutus.html">О нас</a></li>
+			<li><a href="map.html">Карта</a></li>
+		</ul>
+	</nav>
+</header>
+<section id="pages" class="group">
+	<div id="loadcontent" class="group">
+		<nav id="tabs">
+			<ul>
+				<li class="tapped" id="tab_bad_court">Пиздюки</li>
+				<li id="tab_good_court">Красавеллы</li>
+				<li id="tab_dangerous_woman">Опасные тетки</li>
+			</ul>
+		</nav>
+		<section class="courtlist show" id="bad_court">
+			<h2>Пиздюки</h2>
+			<p>Пидарские суды, которые звонят и хотят, чтобы за них сделали всю работу.</p>
+			<ul class="group">
+				<li class="group">
+					<img src="images/courts/chernyahivskiy_tn.jpg" alt="Черняхівський районний суд" />
+					<span class="info">
+						<span class="photoname">Черняхівський районний суд</span>
+						<span class="description">Заступник голови Черняхівського районного суду Житомирської області погорів на хабарі у розмірі 1000 доларів США.</span>
+						<span class="rating"></span>
+					</span>
+				</li>
+				<li class="group">
+					<img src="images/courts/suvorovskiy_tn.jpg" alt="Суворовський районний суд м. Одеса" />
+					<span class="info">
+						<span class="photoname">Суворовський районний суд м. Одеса</span>
+						<span class="description"> На теле Суворовского районного суда г. Одессы имеется такой гнойный прыщ как судья Галий Снежана.</span>
+						<span class="rating"></span>
+					</span>
+				</li>
+				<li class="group">
+					<img src="images/courts/elanetskiy_tn.jpg" alt="Єланецький районний суд" />
+					<span class="info">
+						<span class="photoname">Єланецький районний суд</span>
+						<span class="description">По иску прокуратуры Еланецкого района суд признал за двумя несовершеннолетними право собственности на наследственное имущество — земельный участок площадью почти 6 гектаров стоимостью больше 130 тысяч гривен.</span>
+						<span class="rating"></span>
+					</span>
+				</li>	
+				<li class="group">
+					<img src="images/courts/gorodenkivskiy_tn.jpg" alt="Городенківський районний суд" />
+					<span class="info">
+						<span class="photoname">Городенківський районний суд</span>
+						<span class="description">Суддя Городенківського районного суду Івано-Франківської області Стефурак Євген Миколайович рекомендований на посаду судді апеляційного суду Івано-Франківської області.</span>
+						<span class="rating"></span>
+					</span>
+				</li>						
+			</ul>			
+		</section> <!-- Плохие суды -->
+		<section class="courtlist hide" id="good_court">
+			<h2>Красавеллы</h2>
+			<p>Нормальные суды, которые никогда не морозятся. Ты им сказал как это сделать, они берут и сами делают.</p>
+			<ul class="group">
+				<li class="group">
+					<img src="images/courts/borislavskiy_tn.jpg" alt="Бориславський міський суд" />
+					<span class="info">
+						<span class="photoname">Бориславський міський суд</span>
+						<span class="description">Бориславський міський суд призупинив рішення Бориславської міської ради про зменшення тарифів на воду та водовідведення з 7,99 грн до 5,78 грн.</span>
+						<span class="rating"></span>
+					</span>
+				</li>
+				<li class="group">
+					<img src="images/courts/gorodeshinskiy_tn.jpg" alt="Городищенський районий суд" />
+					<span class="info">
+						<span class="photoname">Городищенський районий суд</span>
+						<span class="description">Городищенський районий суд Черкаської області відпустив під особисте зобов’язання чотирьох автомайданівців. Про це написав голова Черкаської обласної організації партії УДАР Віталій Іляшенко у Фейсбуці.</span>
+						<span class="rating"></span>
+					</span>
+				</li>					
+				<li class="group">
+					<img src="images/courts/ternopolskiy_tn.jpg" alt="Тернопільський міськрайонний суд" />
+					<span class="info">
+						<span class="photoname">Тернопільський міськрайонний суд</span>
+						<span class="description">Тернопільський міськрайонний суд Тернопільської області є судом першої інстанції і розглядає справи, віднесені процесуальним законом до його підсудності: цивільні, кримінальні, адміністративні справи, а також справи про адміністративні правопорушення.</span>
+						<span class="rating"></span>
+					</span>
+				</li>						
+			</ul>
+		</section> <!-- Хорошие суды -->
+		<section class="courtlist hide" id="dangerous_woman">
+			<h2>Опасные тетки</h2>
+			<p>Опасные тетки, которые толком мышкой управлять не умеют. Звонят и кричат, чтобы срочно всё починили.</p>
+			<ul class="group">
+				<li class="group">
+					<img src="images/courts/ichnynskiy_tn.jpg" alt="Ічнянський районний суд" />
+					<span class="info">
+						<span class="photoname">Ічнянський районний суд</span>
+						<span class="description">у Ічнянському районному суді, встановлено систему відеоконференцзв’язку в залах судових засідань з програмною складовою – програмний комплекс TRUECONF.</span>
+						<span class="rating"></span>
+					</span>
+				</li>
+				<li class="group">
+					<img src="images/courts/lenenskiy_tn.jpg" alt="Ленінський районний суд" />
+					<span class="info">
+						<span class="photoname">Ленінський районний суд</span>
+						<span class="description">Ленинский районный суд Кировограда 8 января приговорил уроженца областного центра к 2 годам лишения свободы за отказ от несения воинской службы.</span>
+						<span class="rating"></span>
+					</span>
+				</li>					
+				<li class="group">
+					<img src="images/courts/sribnyanskiy_tn.jpg" alt="Срібнянський районний суд" />
+					<span class="info">
+						<span class="photoname">Срібнянський районний суд</span>
+						<span class="description">В даний час в Срібнянському районному суді працює два  судді –Цигура Ніна Андріївна та Ходіч Валентина Михайлівна . Цигура Ніна Андріївна працює на посаді заступника голови Срібнянського районного суду Чернігівської області. Ходіч Валентина Михайлівна працює суддею Срібнянського районного суду Чернігівської області.</span>
+						<span class="rating"></span>
+					</span>
+				</li>						
+			</ul>
+		</section> <!-- Хорошие суды -->
+	</div>	
+</section>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='assets/js/libs/jquery-1.11.3.min.js'>\x3C/script>")</script>
+<script src="assets/js/functions.js"></script>
+</body>
+</html>
