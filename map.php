7775ef9d4eeba808cb9e42c8767fd6745dea966e
Andrey Kachanov <andrey170749@yandex.ru>
2015-09-27 15:56:11 +0300

    Adding map.php

---
+++ map.php
@@ -0,0 +1,93 @@
+<!doctype html>
+
+<!--
+	HTML5 Reset: https://github.com/murtaugh/HTML5-Reset
+	Free to use
+-->
+
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="dsa">
+	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine (even in intranet) -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DSA</title>
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0"/>
+	<!-- concatenate and minify for production -->
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assets/css/reset.css" />
+	<link rel="stylesheet" href="assets/css/desktop.css" />
+
+	<!-- Target iPhone -->
+	<link rel="stylesheet" href="assets/css/handheld.css" media="(max-device-width:480px)" />
+
+	<!-- Target iPad -->
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-device-width:768px) and (max-device-width:1024px)" />
+
+	<!-- Target Galaxy Tab -->
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-width:1280px) and (max-width:1280px)" />
+	<link rel="stylesheet" href="assets/css/tablet.css" media="(min-width:800px) and (max-width:800px)" />
+
+	<link href='https://fonts.googleapis.com/css?family=Indie+Flower|Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Marck+Script&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Philosopher&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Leckerli+One|Rokkitt:700,400|Luckiest+Guy" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	 <link href='https://fonts.googleapis.com/css?family=Lobster|Marmelad|Bad+Script&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lobster|Marmelad|Bad+Script|Poiret+One&subset=latin,cyrillic' rel='stylesheet' type='text/css'>	
+ 	<link href='https://fonts.googleapis.com/css?family=Istok+Web&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+ 	<link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+ <script src="assets/js/libs/prefixfree.min.js"></script>
+	 <script src="assets/js/libs/modernizr-2.8.3.dev.js"></script>
+</head>
+<body>
+<header>
+	<a class="emblem" href="index.php"><img src="images/emblem_ukr1.png" title="Судова влада України" alt="Судова влада України"></a>
+	<h1><a href="#">Судова влада України</a></h1>
+	<nav id="topnav">
+		<ul>
+			<li><a href="index.php">Суды</a></li>
+			<li><a href="aboutus.php">О нас</a></li>
+			<li><a href="map.php">Карта</a></li>
+		</ul>
+	</nav>
+</header>
+<section id="pages" class="group">
+	<div id="loadcontent" class="group">
+		<section id="map">
+			<p>Мы расположены в чудесном городе Киев, на улице Липской 18/5.</p>
+			<p>Нажмите на карту, чтобы получить направление.</p>
+			<a href="http://maps.google.com/maps?q=4171+Market+St+Ventura+CA">
+				<img src="http://maps.google.com/maps/api/staticmap?center=4171+Market+St+Ventura+CA&amp;zoom=15&amp;size=500x400&amp;format=png32&amp;maptype=roadmap&amp;markers=4171+Market+St+Ventura+CA&amp;sensor=false" />
+			</a>
+			<h3>Hours of Operation</h3>
+			<ul>
+				<li>Monday-Thursdays 9am-9pm</li>
+				<li>Fridays 9am-midnight</li>
+				<li>Sundays Noon-8pm</li>
+			</ul>
+		</section>	
+	</div>	
+</section>
+<footer class="group">
+	<p>01601, м. Київ, вул. Липська 18/5 &#9733; 0-800-500-271</p>
+	<nav id="bottomnav">
+		<ul>
+			<li id="fbmenu"><a href="index.php">Суды</a></li>
+			<li id="fbaboutus"><a href="aboutus.php">О нас</a></li>
+			<li id="fbcallus" class="handheld"><a href="tel:+380800500271">Позвонить</a></li> 
+			<li id="fbmap"><a href="map.php">Карта</a></li>
+		</ul>
+	</nav>
+</footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='assets/js/libs/jquery-1.11.3.min.js'>\x3C/script>")</script>
+<script src="assets/js/functions.js"></script>
+</body>
+</html>
