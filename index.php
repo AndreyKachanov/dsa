87d83994e34ea557261ba24fcce8dc799b566458
Andrey Kachanov <andrey170749@yandex.ru>
2015-10-02 22:56:12 +0300

    Added new courts and new photos of ships

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
  	<link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
  	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
  	<link href='https://fonts.googleapis.com/css?family=PT+Sans:400italic|Bad+Script|Forum|Ubuntu+Mono:400,700italic|Lobster|PT+Sans+Narrow&subset=latin,cyrillic,cyrillic-ext' rel='stylesheet' type='text/css'>
+ 	<link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
  <script src="assets/js/libs/prefixfree.min.js"></script>
 	 <script src="assets/js/libs/modernizr-2.8.3.dev.js"></script>
 </head>
@@ -64,12 +65,12 @@
 	<div id="loadcontent" class="group">
 		<nav id="tabs">
 			<ul>
-				<li class="tapped" id="tab_bad_court">Загальні суди</li>
-				<li id="tab_good_court">Спеціалізовані суди</li>
-				<li id="tab_dangerous_woman">Вищі</li>
+				<li class="tapped" id="tab_general_courts">Загальні</li>
+				<li id="tab_specialized_courts">Спеціалізовані</li>
+				<li id="tab_high_courts">Вищі</li>
 			</ul>
 		</nav>
-		<section class="courtlist show" id="bad_court">
+		<section class="courtlist show" id="general_courts">
 			<h2>Загальні суди</h2>
 			<p>Місцевими загальними судами, відповідно до ст. 21 Закону України "Про судоустрій України", є районні, районні у містах, міські та міськрайонні суди, а також військові суди гарнізонів.</p>
 			<ul class="group">
@@ -77,102 +78,134 @@
 					<img src="images/courts/chernyahivskiy_tn.jpg" alt="Черняхівський районний суд" />
 					<span class="info">
 						<span class="photoname">Черняхівський районний суд</span>
-						<span class="description">Заступник голови Черняхівського районного суду Житомирської області погорів на хабарі у розмірі 1000 доларів США.</span>
-						<span class="rating"></span>
+						<span class="description">У теперішній час Черняхівський районний суд Житомирської області розташовано у двоповерховій будівлі за адресою: Житомирська область, Черняхівський районний суд, смт. Черняхів, вул. Червоноармійська, 1.</span>
+						<span class="rating">(04134) 4-13-46</span>
 					</span>
 				</li>
 				<li class="group">
 					<img src="images/courts/suvorovskiy_tn.jpg" alt="Суворовський районний суд м. Одеса" />
 					<span class="info">
 						<span class="photoname">Суворовський районний суд</span>
-						<span class="description"> На теле Суворовского районного суда г. Одессы имеется такой гнойный прыщ как судья Галий Снежана.</span>
-						<span class="rating"></span>
+						<span class="description">На сьогоднішній  день Суворовський районний суд м. Одеси розглядає цивільні, кримінальні, адміністративні справи та справи про адміністративні правопорушення в порядку першої інстанції. Штат суду складається з 117 осіб.</span>
+						<span class="rating">(048) 785-68-37</span>
 					</span>
 				</li>
 				<li class="group">
 					<img src="images/courts/gorodeshinskiy_tn.jpg" alt="Городищенський районий суд" />
 					<span class="info">
 						<span class="photoname">Городищенський районий суд</span>
-						<span class="description">Городищенський районий суд Черкаської області відпустив під особисте зобов’язання чотирьох автомайданівців. Про це написав голова Черкаської обласної організації партії УДАР Віталій Іляшенко у Фейсбуці.</span>
-						<span class="rating"></span>
+						<span class="description">Відповідно до положень Закону України " Про внесення змін та доповнень до Закону України " Про судоустрій " прийнятого 24 лютого 1994р. Городищенський районний народний суд став назватися Городищенським районним судом.</span>
+						<span class="rating">(04734) 2-05-81</span>
 					</span>
 				</li>	
 				<li class="group">
 					<img src="images/courts/gorodenkivskiy_tn.jpg" alt="Городенківський районний суд" />
 					<span class="info">
 						<span class="photoname">Городенківський районний суд</span>
-						<span class="description">Суддя Городенківського районного суду Івано-Франківської області Стефурак Євген Миколайович рекомендований на посаду судді апеляційного суду Івано-Франківської області.</span>
-						<span class="rating"></span>
+						<span class="description">Зараз в Городенківському районному суді загальна чисельність становить 24  працівники, з них: 4 судді, 18 державних службовців і 2 прибиральниці.</span>
+						<span class="rating">(03430) 2-27-86</span>
 					</span>
 				</li>
 				<li class="group">
 					<img src="images/courts/borislavskiy_tn.jpg" alt="Бориславський міський суд" />
 					<span class="info">
 						<span class="photoname">Бориславський міський суд</span>
-						<span class="description">Бориславський міський суд призупинив рішення Бориславської міської ради про зменшення тарифів на воду та водовідведення з 7,99 грн до 5,78 грн.</span>
-						<span class="rating"></span>
+						<span class="description">Бориславський міський суд Львівської області розглядає кримінальні, цивільні, адміністративні справи, а також справи про адміністративні правопорушення у випадках та порядку, передбачених процесуальним законом.</span>
+						<span class="rating">(03248) 5-20-44</span>
 					</span>
 				</li>						
 			</ul>			
 		</section> <!-- Плохие суды -->
-		<section class="courtlist hide" id="good_court">
+		<section class="courtlist hide" id="specialized_courts">
 			<h2>Спеціалізовані суди</h2>
-			<p>Місцевими господарськими судами є господарські суди областей, міста Києва, а місцевими адміністративними судами є окружні суди, що утворюються в округах відповідно до указу Президента України.</p>
+			<p>Місцевими господарськими судами є господарські суди областей, міста Києва, а місцевими адміністративними судами є окружні суди, що утворюються в округах відповідно до указу Президента.</p>
 			<ul class="group">
 				<li class="group">
-					<img src="images/courts/borislavskiy_tn.jpg" alt="Бориславський міський суд" />
+					<img src="images/courts/donetsk_gospod_tn.jpg" alt="Господарський суд Донецької області" />
 					<span class="info">
-						<span class="photoname">Бориславський міський суд</span>
-						<span class="description">Бориславський міський суд призупинив рішення Бориславської міської ради про зменшення тарифів на воду та водовідведення з 7,99 грн до 5,78 грн.</span>
-						<span class="rating"></span>
+						<span class="photoname">Господарський суд Донецької області</span>
+						<span class="description">Це місцевий спеціалізований господарський суд першої інстанції загальної юрисдикції, розташований в місті Донецьку, юрисдикція якого поширюється на Донецьку область.</span>
+						<span class="rating">(057) 702-01-09</span>
 					</span>
 				</li>
 				<li class="group">
-					<img src="images/courts/gorodeshinskiy_tn.jpg" alt="Городищенський районий суд" />
+					<img src="images/courts/harkov_gospod_tn.jpg" alt="Господарський суд Харківської області" />
 					<span class="info">
-						<span class="photoname">Городищенський районий суд</span>
-						<span class="description">Городищенський районий суд Черкаської області відпустив під особисте зобов’язання чотирьох автомайданівців. Про це написав голова Черкаської обласної організації партії УДАР Віталій Іляшенко у Фейсбуці.</span>
-						<span class="rating"></span>
+						<span class="photoname">Господарський суд Харківської області</span>
+						<span class="description">Це місцевий спеціалізований господарський суд першої інстанції загальної юрисдикції, розташований в місті Донецьку, юрисдикція якого поширюється на Харківську область.</span>
+						<span class="rating">(057) 703-47-19</span>
 					</span>
 				</li>					
 				<li class="group">
-					<img src="images/courts/ternopolskiy_tn.jpg" alt="Тернопільський міськрайонний суд" />
+					<img src="images/courts/poltava_gospod_tn.jpg" alt="Господарський суд Полтавської області" />
 					<span class="info">
-						<span class="photoname">Тернопільський міськрайонний суд</span>
-						<span class="description">Тернопільський міськрайонний суд Тернопільської області є судом першої інстанції і розглядає справи, віднесені процесуальним законом до його підсудності: цивільні, кримінальні, адміністративні справи, а також справи про адміністративні правопорушення.</span>
-						<span class="rating"></span>
+						<span class="photoname">Господарський суд Полтавської області</span>
+						<span class="description">Це місцевий спеціалізований господарський суд першої інстанції загальної юрисдикції, розташований в місті Донецьку, юрисдикція якого поширюється на Полтавську область.</span>
+						<span class="rating">(0532) 56-45-95 </span>
 					</span>
-				</li>						
+				</li>
+				<li class="group">
+					<img src="images/courts/zakarpatskiy_admin_tn.jpg" alt="Закарпатський окружний адміністративний суд" />
+					<span class="info">
+						<span class="photoname">Закарпатський окружний адміністративний суд</span>
+						<span class="description">Це місцевий спеціалізований адміністративний суд першої інстанції загальної юрисдикції, розташований у місті Одесі, юрисдикція якого поширюється на Закарпатську область.</span>
+						<span class="rating">(0312) 64-07-61</span>
+					</span>
+				</li>
+				<li class="group">
+					<img src="images/courts/volyn_admin_tn.jpg" alt="Волинський окружний адміністративний суд" />
+					<span class="info">
+						<span class="photoname">Волинський окружний адміністративний суд</span>
+						<span class="description">Це місцевий спеціалізований адміністративний суд першої інстанції загальної юрисдикції, розташований у місті Одесі, юрисдикція якого поширюється на Волинську область.</span>
+						<span class="rating">(0332) 72-78-72</span>
+					</span>
+				</li>								
 			</ul>
 		</section> <!-- Хорошие суды -->
-		<section class="courtlist hide" id="dangerous_woman">
+		<section class="courtlist hide" id="high_courts">
 			<h2>Вищі суди</h2>
 			<p>У вищому спеціалізованому суді можуть утворюватися судові палати з розгляду окремих категорій справ за визначеною спеціалізацією в межах відповідної спеціалізації судової юрисдикції.</p>
 			<ul class="group">
 				<li class="group">
-					<img src="images/courts/ichnynskiy_tn.jpg" alt="Ічнянський районний суд" />
+					<img src="images/courts/vishiy_admin_tn.jpg" alt="Вищий адмiнiстративний суд України" />
 					<span class="info">
-						<span class="photoname">Ічнянський районний суд</span>
-						<span class="description">у Ічнянському районному суді, встановлено систему відеоконференцзв’язку в залах судових засідань з програмною складовою – програмний комплекс TRUECONF.</span>
-						<span class="rating"></span>
+						<span class="photoname">Вищий адмiнiстративний суд України</span>
+						<span class="description">Вищий адміністративний суд України діє як суд першої інстанції по конкретних категоріях справ, а також як суд апеляційної та касаційної інстанцій.</span>
+						<span class="rating">(044) 501-95-25</span>
 					</span>
 				</li>
 				<li class="group">
-					<img src="images/courts/lenenskiy_tn.jpg" alt="Ленінський районний суд" />
+					<img src="images/courts/vishiy_gospod_tn.jpg" alt="Вищий господарський суд України" />
 					<span class="info">
-						<span class="photoname">Ленінський районний суд</span>
-						<span class="description">Ленинский районный суд Кировограда 8 января приговорил уроженца областного центра к 2 годам лишения свободы за отказ от несения воинской службы.</span>
-						<span class="rating"></span>
+						<span class="photoname">Вищий господарський суд України</span>
+						<span class="description">Вищий господарський суд України — вищий спеціалізований суд України і суд касаційної інстанції з розгляду господарських справ.</span>
+						<span class="rating">(044) 536-18-01</span>
 					</span>
 				</li>					
 				<li class="group">
-					<img src="images/courts/sribnyanskiy_tn.jpg" alt="Срібнянський районний суд" />
+					<img src="images/courts/vishiy_special_tn.jpg" alt="Вищий спеціалізований суд України з розгляду цивільних і кримінальних справ" />
 					<span class="info">
-						<span class="photoname">Срібнянський районний суд</span>
-						<span class="description">В даний час в Срібнянському районному суді працює два  судді –Цигура Ніна Андріївна та Ходіч Валентина Михайлівна . Цигура Ніна Андріївна працює на посаді заступника голови Срібнянського районного суду Чернігівської області. Ходіч Валентина Михайлівна працює суддею Срібнянського районного суду Чернігівської області.</span>
-						<span class="rating"></span>
+						<span class="photoname">Вищий спеціалізований суд України з розгляду цивільних і кримінальних справ</span>
+						<span class="description">Вищий спеціалізований суд України з розгляду цивільних і кримінальних справ — суд касаційної інстанції для цивільних і кримінальних справ в системі судів загальної юрисдикції України.</span>
+						<span class="rating">(044) 591-10-00</span>
 					</span>
-				</li>						
+				</li>
+				<li class="group">
+					<img src="images/courts/verhovniy_tn.jpg" alt="Верховний Суд України" />
+					<span class="info">
+						<span class="photoname">Верховний Суд України</span>
+						<span class="description">Верховний Суд України — найвищий судовий орган в українській системі судів загальної юрисдикції. Здійснює правосуддя, забезпечує єдність судової практики у порядку та спосіб, визначені процесуальним законом.</span>
+						<span class="rating">(044) 253-33-13</span>
+					</span>
+				</li>	
+				<li class="group">
+					<img src="images/courts/konstituc_tn.jpg" alt="Конституційний Суд України" />
+					<span class="info">
+						<span class="photoname">Конституційний Суд України</span>
+						<span class="description">Конституційний Суд України — єдиний орган конституційної юрисдикції в Україні, завданням якого є гарантування верховенства Конституції України як Основного Закону держави на всій території України.</span>
+						<span class="rating">(044) 253-02-87</span>
+					</span>
+				</li>							
 			</ul>
 		</section> <!-- Хорошие суды -->
 	</div>	
